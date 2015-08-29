86b5a5fd4da922d88e74f94b7b448bb0c6af635a
Ryne Emory <ryneemory@gmail.com>
2015-08-29 10:12:17 -0600

    express app generated, heroku created, git setup

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Gross Wedding 6-18-16' });
+});
+
+module.exports = router;
