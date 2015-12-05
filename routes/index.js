6f66666f1627b1dcfb95ff916d45cd543beb6fb9
Ryne Emory <ryneemory@gmail.com>
2015-12-04 19:03:05 -0700

    major overhaul

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gross Wedding' });
+  res.render('index', {title: 'Gross Wedding' });
 });
 
 router.get('/cabins', function(req, res, next) {
   res.render('cabins', {});
 });
+
+router.get('/lodging', function(req, res, next) {
+  res.render('lodging', {});
+})
+
+router.get('/todo', function(req, res, next) {
+  res.render('todo', {});
+})
+
+router.get('/getting', function(req, res, next) {
+  res.render('getting', {});
+})
 
 module.exports = router;
