96370d44e501f357d843cc8d28c3aa18d27e9192
Ryne Emory <ryneemory@gmail.com>
2016-02-22 20:22:35 -0700

    fix for bug with mongo error

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')(process.env.MONGOLAB_URI);
+var db = require('monk')('localhost/guests');
 var rsvp = db.get('rsvp');
 
 /* GET users listing. */
